d4bf930adb9581ca29e73648100a98792dd85cfa
James Shkolnik <james.shkolnik@gusto.com>
2019-12-21 23:26:52 -0600

    Cleanup spec

--- spec/integration/ruby_server_js_client_spec.rb
+++ spec/integration/ruby_server_js_client_spec.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
 
 describe 'connecting to a ruby server from a javascript client', type: :feature do
-  let(:session) { Capybara.current_session }
-  let(:server) { session.server }
+  let(:server) { Capybara.current_session.server }
+  let(:test_page) { File.expand_path('../js-client/test.html', __dir__) }
   let(:name) { 'James' }
+  
   let(:js_script) do
     <<-EOF
     var helloService = new HelloServiceClient('http://#{server.host}:#{server.port}', null, null);
@@ -14,7 +15,7 @@ describe 'connecting to a ruby server from a javascript client', type: :feature 
   end
 
   it 'returns the expected response from the service' do
-    visit "file://#{File.expand_path('../js-client/test.html', __dir__)}"
+    visit "file://#{test_page}"
     execute_script(js_script)
     sleep 1
     result = evaluate_script("window.grpcResponse.toObject()")
