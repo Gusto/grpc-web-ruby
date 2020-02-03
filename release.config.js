2c35dc082be1109436514e8ae95b065d380cb51c
Shayon Mukherjee <shayon.mukherjee@gusto.com>
2020-02-03 08:31:38 -0800

    Add prepare and publish bins

--- release.config.js
+++ release.config.js
@@ -4,6 +4,17 @@ module.exports = {
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     [
+      '@semantic-release/exec',
+      {
+        // TODO: Verify the presence of the GEM_HOST_API_KEY env var
+        // verifyConditionsCmd: './verify.sh',
+        // eslint-disable-next-line no-template-curly-in-string
+        prepareCmd: 'ruby bin/prepare.rb ${nextRelease.version}',
+        // eslint-disable-next-line no-template-curly-in-string
+        publishCmd: 'ruby bin/publish.rb ${nextRelease.version}',
+      },
+    ],
+    [
       '@semantic-release/git',
       {
         message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
