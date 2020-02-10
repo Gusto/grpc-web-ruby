81fb4fe63cccd3d33cac857b2034ee3e06cd25c7
Shayon Mukherjee <shayonj@users.noreply.github.com>
2020-02-10 09:48:14 -0800

    Add semantic-release (#8)
    * Add semantic-release
    
    Release automatio
    
    * Add prepare and publish bins
    
    * Update published gem

---
+++ bin/prepare.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+def set_version
+  new_version = ARGV[0]
+
+  contents = File.read('lib/grpc_web/version.rb')
+
+  new_contents = contents.gsub(/VERSION = '[0-9.]*'/, "VERSION = '#{new_version}'")
+  File.write('lib/grpc_web/version.rb', new_contents)
+end
+
+def bundle_install
+  system('bundle install')
+end
+
+def build_gem
+  system('gem build grpc-web.gemspec')
+end
+
+set_version
+bundle_install
+build_gem
