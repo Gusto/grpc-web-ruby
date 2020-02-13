e6afe3ff21dca1dab59a4fd8e950644feccdc0e9
noaelad <noa.elad@gusto.com>
2020-02-13 15:52:13 -0800

    add unicode

--- spec/unit/grpc_web/message_framing_spec.rb
+++ spec/unit/grpc_web/message_framing_spec.rb
@@ -3,21 +3,18 @@
 require 'grpc_web/message_framing'
 
 RSpec.describe GRPCWeb::MessageFraming do
-  let(:unframed_data) { 'some content' }
-  let(:payload_framed_data) { "\x00\x00\x00\x00\fsome content".b }
-  let(:header_framed_data) { "\x80\x00\x00\x00\fsome content".b }
   let(:unpacked_frames) {
     [
       ::GRPCWeb::MessageFrame.header_frame('data in the header'),
-      ::GRPCWeb::MessageFrame.payload_frame('data in the first frame'),
+      ::GRPCWeb::MessageFrame.payload_frame('data in the \u1f61d first frame'),
       ::GRPCWeb::MessageFrame.payload_frame('data in the second frame')
     ]
   }
   let(:packed_frames) do
     string = "\x80\x00\x00\x00\x12data in the header" +
-      "\x00\x00\x00\x00\x17data in the first frame" +
+      "\x00\x00\x00\x00\x1Fdata in the \\u1f61d first frame" +
       "\x00\x00\x00\x00\x18data in the second frame"
-    string.b
+    string.b # encode to ASCII-8BIT
   end
   describe '#frame_content' do
     subject { described_class.pack_frames(unpacked_frames) }
