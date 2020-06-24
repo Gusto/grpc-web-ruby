cf2ca928631559af3631b57bd41cc4ba15c0c446
noaelad <noa.elad@gusto.com>
2020-06-24 12:41:28 -0700

    undo rubocop autofix

--- entrypoint.rb
+++ entrypoint.rb
@@ -9,7 +9,7 @@ require 'net/http'
     http = Net::HTTP.new('envoy', 8080)
     response = http.request(Net::HTTP::Get.new('/ready'))
     break if response.body != 'no healthy upstream'
-  rescue StandardError
+  rescue StandardError # rubocop:disable Lint/SuppressedException
     # Intentionally empty, will retry
   end
   sleep 0.1
